8804a739c08209b709bac33c6c2e72027c51fd96
Lucas Klein <59050136+lectair@users.noreply.github.com>
2020-07-26 19:47:55 +0000

    Update plsbeg.py

--- plsbeg.py
+++ plsbeg.py
@@ -4,6 +4,8 @@ import datetime
 from colorama import init, Fore, Back, Style
 init()
 
+no = 0
+
 def getTime():
     now = datetime.datetime.now()
     if now.hour < 10:
@@ -21,24 +23,65 @@ def getTime():
     time = Fore.YELLOW+'['+str(hours)+':'+str(minutes)+':'+str(seconds)+']'+Style.RESET_ALL
     return time
 
-no = 1
-print(f'{getTime()} Started!\nWaiting 5 seconds...\n')
+def plsbal():
+	p.write("pls bal")
+	p.press("enter")
+	print(f'{getTime() + Style.BRIGHT + Fore.GREEN} Pls balance!{Style.RESET_ALL}')
+
+def plsbeg():
+	p.write("pls beg")
+	p.press("enter")
+	print(f'{getTime() + Style.BRIGHT + Fore.GREEN} Pls beg!{Style.RESET_ALL}')
+
+def plsbet():
+	p.write("pls bet 150") # Modify this number according to the risk that you want for bets.
+	p.press("enter")
+	print(f'{getTime() + Style.BRIGHT + Fore.MAGENTA} Pls bet!{Style.RESET_ALL}')
+
+def plsdeposit():
+	p.write("pls dep 500")
+	p.press("enter")
+	print(f'{getTime() + Style.BRIGHT + Fore.GREEN} Trying to deposit 500 coins!{Style.RESET_ALL}')
+
+def plsdaily():
+	p.write("pls daily")
+	p.press("enter")
+	print(f'{getTime() + Fore.BLUE} Trying to pick daily bonus...{Style.RESET_ALL}')
+
+def plslottery():
+	p.write("pls lottery")
+	p.press("enter")
+	t.sleep(2)
+	p.write("yes")
+	p.press("enter")
+	print(f'{getTime() + Fore.BLUE} Trying to enter lottery...{Style.RESET_ALL}')
+
+def plswork():
+	t.sleep(2)
+	p.write("pls work")
+	p.press("enter")
+	print(f'{getTime() + Fore.BLUE} Trying to work...{Style.RESET_ALL}')
+
+print(f'{getTime() + Style.BRIGHT + Fore.CYAN} Started!\nWaiting 5 seconds...\n{Style.RESET_ALL}')
 t.sleep(5)
+plsbal()
+plsdaily()
+
+# You can change this as you want!
 while True:
-	if no == 3:
-		p.write("pls beg")
-		p.press("enter")
-		print(f'{getTime()} Pls beg!')
+	plslottery()
+	plswork()
+	for i in range(30):
 		for i in range(3):
-			p.write("pls bet 150")
-			p.press("enter")
-			print(f'{getTime()} Pls bet!')
-			t.sleep(10.15)
-		t.sleep(10)
-		no = 1
-	else:
-		p.write("pls beg")
-		p.press("enter")
-		print(f'{getTime()} Pls beg!')
-		t.sleep(40.15)
-	no += 1
+			if no == 8: # Increase or decrease this number in order to change the frequency of bets [Higher number = Less bets].
+				plsbeg()
+				for i in range(3):
+					plsbet()
+					t.sleep(10.4)
+				t.sleep(10)
+				no = 0
+			else:
+				plsbeg()
+				t.sleep(40.4)
+			no += 1
+		plsdeposit()
